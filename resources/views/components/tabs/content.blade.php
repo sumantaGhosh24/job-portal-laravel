3bbacb9ace216734e9a053ff08bf6f0373c7ac67
sumanta ghosh <84187999+sumantaGhosh24@users.noreply.github.com>
2025-11-07 20:41:39 +0530

    clean files for implement livewire

--- resources/views/components/tabs/content.blade.php
+++
@@ -1,5 +0,0 @@
-@props(['tab'])
-
-<div x-cloak x-show="tab === '{{ $tab }}'" x-transition.opacity>
-    {{ $slot }}
-</div>
--- resources/views/components/text-input.blade.php
+++
@@ -1,3 +0,0 @@
-@props(['disabled' => false])
-
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'mb-2 w-full px-4 py-2 rounded-md border border-gray-300']) }}>
